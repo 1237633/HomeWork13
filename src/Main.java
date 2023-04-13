b9ba0ccf4c6783f531b1e5ad3c4a9e068fb51988
ВасянПупкин <tgoroshek@gmail.com>
2023-04-13 16:03:05 +0300

    HW Finished

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,13 @@ public class Main {
         GriffendorStudents Hermoina = new GriffendorStudents("Hermiona Granger", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
         GriffendorStudents Ron = new GriffendorStudents("Ron Whisley", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
 
-
         SlizerinStudents Drako = new SlizerinStudents("Drako Malfoy", rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(20) + 80, rnd.nextInt(65) + 35, rnd.nextInt(20) + 80);
         SlizerinStudents Graham = new SlizerinStudents("Graham Montegru", rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(20) + 80, rnd.nextInt(65) + 35, rnd.nextInt(20) + 80);
         SlizerinStudents Gregory = new SlizerinStudents("Gregory Goil", rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(65) + 35, rnd.nextInt(20) + 80, rnd.nextInt(65) + 35, rnd.nextInt(20) + 80);
 
-
         PuffendoyStudents JustinBieber = new PuffendoyStudents("Justin Finch-Fletchley", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
         PuffendoyStudents Zahar = new PuffendoyStudents("Ded Zahar", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
         PuffendoyStudents CedricDiggori = new PuffendoyStudents("Cedric Diggori", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
-
 
         RavenclawStudents chinaGuy = new RavenclawStudents("Chinese Name", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
         RavenclawStudents PadmaPatil = new RavenclawStudents("Padma Patil", rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50, rnd.nextInt(50) + 50);
